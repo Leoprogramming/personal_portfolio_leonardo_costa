41ca983849d9c06aaffc1faf107a4ac49d7d2d66
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-15 20:56:21 +0100

    imported landingPage, aboutMe, Contact, Projects and Resume to main.js

--- my-personal-portfolio/src/components/main.js
+++ my-personal-portfolio/src/components/main.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import LandingPage from './landingPage';
+import AboutMe from './aboutMe';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
 
 const Main = () => (
   <switch>
