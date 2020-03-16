1b40bc0c885802eb0a8d1723d1eca33fb1285371
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-16 01:04:07 +0100

    adding pavatar picture

--- my-personal-portfolio/src/components/landingPage.js
+++ my-personal-portfolio/src/components/landingPage.js
@@ -7,7 +7,12 @@ class Landing extends Component {
       // <div style={{width: '100%', margin: 'auto'}}>
       <div className="landing-div">
         <Grid className="landing-grid">
-          <Cell col={12}></Cell>
+          <Cell col={12}>
+            <img
+            src="https://www.shareicon.net/avatar-profile-user-avatar-social-user-profiles-avatar-649770" 
+            alt="Avatar Profile"
+            className="avatar-img"/>
+          </Cell>
         </Grid>
       </div>
     );
