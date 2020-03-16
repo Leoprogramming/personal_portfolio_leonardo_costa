f7a0a5977c163cb3925ad82c7a3a6c97efb38ffa
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-16 16:42:43 +0100

    added banner-text and styling

--- my-personal-portfolio/src/components/landingPage.js
+++ my-personal-portfolio/src/components/landingPage.js
@@ -17,6 +17,8 @@ class Landing extends Component {
 
             <div className="banner-text">
               <h1>Full-Stack Web Developer</h1>
+              <hr/>
+              <p>HTML/CSS | Bootstrap | JavaScript | React | Ruby/Rails | NodeJS | Express | MongoDB</p>
             </div>
           </Cell>
         </Grid>
