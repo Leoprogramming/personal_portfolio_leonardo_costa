37fa0a0fb13d09be3cd0b7a95bd5f71f8c89aea1
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-16 19:33:41 +0100

    created toggleCategories

--- my-personal-portfolio/src/components/projects.js
+++ my-personal-portfolio/src/components/projects.js
@@ -7,15 +7,47 @@ class Projects extends Component {
     this.state = { activeTab: 0};
   }
 
+  toggleCategories(){
+    if (this.state.activeTab === 0) {
+      return(
+        <div>
+          <h1>This is React</h1>
+        </div>
+      )
+    } else if(this.state.activeTab === 1){
+      return(
+      <div>
+        <h1>This is Ruby</h1>
+      </div>
+      )
+    } else if(this.state.activeTab === 2) {
+      return(
+      <div>
+        <h1>This is JavaScript</h1>
+      </div>
+      ) 
+    } else if(this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      )
+    }
+  }
+
   render() {
     return(
       <div className="category-tabs">
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
           <Tab>React</Tab>
-          <Tab>Angular</Tab>
-          <Tab>VueJs</Tab>
+          <Tab>Ruby/Rails</Tab>
+          <Tab>JavaScript</Tab>
           <Tab>MongoDB</Tab>
         </Tabs>
+
+        <section className="projects-grid">
+          {this.toggleCategories()}
+        </section>
       </div>
     );
   }
