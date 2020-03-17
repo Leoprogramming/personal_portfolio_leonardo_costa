8c234e3ee28b9f09fd3cc389c8f7657592b4d268
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-17 23:14:43 +0100

    added google fonts, imported Grid/Cell, profilePictureFull

--- my-personal-portfolio/src/components/contact.js
+++ my-personal-portfolio/src/components/contact.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import profilePictureFull from '../profilePictureFull.jpg'
 
 class Contact extends Component {
   render() {
     return(
-      <div>
-        <h1>Contact Page</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Leo Costa</h2>
+            <img className="profile-picture-full" src={profilePictureFull} alt="" />
+          </Cell>
+          <Cell col={6}>
+            
+          </Cell>
+        </Grid>
       </div>
     );
   }
