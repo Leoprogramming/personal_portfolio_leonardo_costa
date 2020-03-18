1de9c066def559337b3b1822900ed63caba679bf
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-18 12:38:34 +0100

    changed font to Roboto and added to contact-grid h2

--- my-personal-portfolio/src/components/contact.js
+++ my-personal-portfolio/src/components/contact.js
@@ -9,11 +9,13 @@ class Contact extends Component {
         <Grid className="contact-grid">
           <Cell col={6}>
             <h2>Leo Costa</h2>
-            <img className="profile-picture-full" src={profilePictureFull} alt="" />
+            <img className="profile-picture-full" src={profilePictureFull} alt="avatar" />
+          <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima doloribus quisquam corrupti aperiam, iure tenetur id similique neque dolorem odio laudantium voluptates eligendi consequatur fuga nisi, nobis deleniti modi rerum?</p>
           </Cell>
+          
           <Cell col={6}>
-            
           </Cell>
+            
         </Grid>
       </div>
     );
