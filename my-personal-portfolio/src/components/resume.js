b378e566c6e8d35b5ec385927d7d57c7bf079098
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-18 16:06:47 +0100

    created education.js file, component and added it to resume.js

--- my-personal-portfolio/src/components/resume.js
+++ my-personal-portfolio/src/components/resume.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import profilePicture from '../profilePicture.jpg';
+import Education from './education';
 
 class Resume extends Component {
   render() {
@@ -30,8 +31,13 @@ class Resume extends Component {
             <p>mywebsite.com</p>
             <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
           </Cell>
-          <Cell className="resume-right-col" col={8}>Right Side</Cell>
+
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+            <Education />
+          </Cell>
         </Grid>
+
       </div>
     );
   }
