85b5f40d59878a9afb916366c9624ee06acf410e
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-18 14:32:46 +0100

    imported and added avatar-img to resume.js

--- my-personal-portfolio/src/components/resume.js
+++ my-personal-portfolio/src/components/resume.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import profilePicture from '../profilePicture.jpg';
 
 class Resume extends Component {
   render() {
     return(
       <div>
         <Grid>
-          <Cell col={4}>Left Side</Cell>
+          <Cell col={4}>
+            <div style={{ textAlign: 'center' }}>
+              <img className="avatar-img"
+              src={profilePicture} 
+              alt="avatar" 
+              style={{ height: '200px' }}
+              />
+            </div>
+            <h2 style={{ paddingTop: '2em' }}>Leo Costa</h2>
+            <h4 style={{ color: 'grey' }}>Full-Stack Developer</h4>
+          </Cell>
           <Cell className="resume-right-col" col={8}>Right Side</Cell>
         </Grid>
       </div>
