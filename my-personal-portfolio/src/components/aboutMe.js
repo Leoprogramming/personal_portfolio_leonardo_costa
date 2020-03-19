1bf80d8b28e071d8444d5cc5beb363a9a1abf0cf
Leonardo Costa <leo.costa.programming@gmail.com>
2020-03-19 12:34:33 +0100

    commented out aboutMe content

--- my-personal-portfolio/src/components/aboutMe.js
+++ my-personal-portfolio/src/components/aboutMe.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class About extends Component {
-  render() {
-    return(
-      <div>
-        <h1>About Page</h1>
-      </div>
-    );
-  }
-}
+// class About extends Component {
+//   render() {
+//     return(
+//       <div>
+//         <h1>About Page</h1>
+//       </div>
+//     );
+//   }
+// }
 
-export default About;
+// export default About;
